cbd832ec1b540d2a788ea74cf6a33918195324a5
pervejislam2404 <pervejislam2404@gmail.com>
2021-12-18 21:02:50 +0600

    update

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ function App() {
           </Route>
 
           <Route path="/home" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
 
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
